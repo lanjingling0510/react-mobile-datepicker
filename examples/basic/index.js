b6d6b5e2f72e95f78bdba370d6426be8e5ab054f
rainie <chenyutian0510@gmail.com>
2018-05-12 10:57:13 +0800

    ✨  [feature] map month number to month name(https://github.com/lanjingling0510/react-mobile-datepicker/issues/26)

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -25,6 +25,21 @@ import DatePicker from '../../lib/index';
         }
 
         render() {
+            const monthMap = {
+                '01': 'Jan',
+                '02': 'Feb',
+                '03': 'Mar',
+                '04': 'Apr',
+                '05': 'May',
+                '06': 'Jun',
+                '07': 'Jul',
+                '08': 'Aug',
+                '09': 'Sep',
+                '10': 'Oct',
+                '11': 'Nov',
+                '12': 'Dec',
+            };
+
             return (
                 <div className="App">
                     <p className="select-time ">
@@ -60,7 +75,7 @@ import DatePicker from '../../lib/index';
                     <DatePicker
                         value={this.state.time}
                         dateSteps={[1, 1, 5]}
-                        dateFormat={['hh', 'mm', 'ss']}
+                        dateFormat={['YYYY', ['MM', (month) => monthMap[month]], 'DD']}
                         theme={this.state.theme}
                         isOpen={this.state.isOpen}
                         onSelect={this.handleSelect}
